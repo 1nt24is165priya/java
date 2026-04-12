d13012301bba8448676221d108f857d74e2d9f5d
1nt24is165priya <1nt24is165.priya@nmit.ac.in>
2026-04-12 11:00:35 +0530

    Added Printing Characters Pattern program

---
+++ Character_pattern.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+ public class Character_pattern{
+    public static void main(String args[]){
+        //Scanner sc = new Scanner(System.in);
+       // int n = sc.nextInt();
+       int n = 4;
+       char ch = 'A';
+        for(int line= 1; line <= n; line++){
+            for(int i=1;i<=line;i++){
+
+                System.out.print(ch);
+                ch++;
+            }
+            System.out.println();
+        }
+    }
+}
