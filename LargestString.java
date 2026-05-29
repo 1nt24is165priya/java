639116a3bd34a1bf4100f36235e7638651d56989
Priya R C <1nt24is165.priya@nmit.ac.in>
2026-05-29 15:05:43 +0530

    Finding the largest string

---
+++ LargestString.java
@@ -0,0 +1,18 @@
+public class LargestString{
+    
+
+    public static void main(String args[]){
+        String strs[] = {"apple", "mango", "banana"};
+        String largest = strs[0];
+         for(int i =1; i<strs.length ; i++){
+             if(largest.compareTo(strs[i]) < 0){
+                largest = strs[i];
+             }
+        }
+
+        System.out.println(largest);
+
+    
+
+    }
+}
